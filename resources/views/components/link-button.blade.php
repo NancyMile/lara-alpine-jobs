506c567c1e3cd3ba14913d4c26b6ffc94b277808
NancyMile <smithnancy@outlook.com>
2024-02-01 02:17:25 +0000

    job page, job-card and link-button components

---
+++ resources/views/components/link-button.blade.php
@@ -0,0 +1,3 @@
+<a href="{{ $href }}" class="border border-slate-300 bg-white text-center font font-semibold text-black shadow-sm rounded-md px-2 py-1 text-sm hover:bg-slate-100 hover:cursor-pointer">
+    {{ $slot }}
+</a>
