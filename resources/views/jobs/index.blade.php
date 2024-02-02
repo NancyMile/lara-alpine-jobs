99f61ecd596c13186eb39857bfc4ab7c99f22ebe
NancyMile <smithnancy@outlook.com>
2024-02-02 03:50:36 +0000

    job filtering, clearing input

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -2,21 +2,21 @@
     <x-breadcrumb :links="['Jobs' => route('jobs.index')]" class="mb-4"/>
 
     <x-card class="mb-4 text-sm ">
-        <form action="{{ route('jobs.index') }}" method="GET">
+        <form id="form-filtering" action="{{ route('jobs.index') }}" method="GET">
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     <div class="mb-1 font-semibold">
                         Search
                     </div>
-                    <x-text-input name="search" value="{{ request('search') }}" placeholder="search"/>
+                    <x-text-input name="search" value="{{ request('search') }}" placeholder="search" form-id="form-filtering"/>
                 </div>
                 <div>
                     <div class="mb-1 font-semibold">
                         Salary
                     </div>
                     <div class="flex space-x-2">
-                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="from" />
-                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="to" />
+                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="from" form-id="form-filtering" />
+                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="to" form-id="form-filtering" />
                     </div>
                 </div>
                 <div>
