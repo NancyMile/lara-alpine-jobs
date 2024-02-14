86472bae55c3ce5b361d5add3a18abcdc528da0f
NancyMile <smithnancy@outlook.com>
2024-02-14 14:42:41 +1100

    employee factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Job::factory(100)->create();
+        \App\Models\User::factory(300)->create();
+
+        $users = \App\Models\User::all()->shuffle(); //randomize the elements
+
+        //create some employers
+        for($i = 0; $i < 20; $i++){
+            \App\Models\Employer::factory()->create([
+                'user_id' => $users->pop()->id //return an user nad remove it from the list of users
+            ]);
+        }
+
+        $employers = \App\Models\Employer::all();
+
+        //create some jobs
+        for($i = 0; $i < 100; $i++){
+            \App\Models\Job::factory()->create([
+                'employer_id' => $employers->random()->id //random employer
+            ]);
+        }
+
         // \App\Models\User::factory(10)->create();
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
