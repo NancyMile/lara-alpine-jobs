70ece5ea777a0750a3951a604611912726937df9
NancyMile <smithnancy@outlook.com>
2024-02-14 12:59:08 +1100

    refactoring  filter backend

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -12,18 +12,8 @@ class JobController extends Controller
      */
     public function index()
     {
-        $jobs = Job::query();
-        $jobs->when(request('search'), function($query){
-            $query->where( function($query){
-                $query->where('title','like', '%'.request('search').'%')
-                    ->orWhere('description','like','%'.request('search').'%');
-            });
-        })->when(request('min_salary'), fn($query) => $query->where('salary','>=', request('min_salary'))
-            )->when(request('max_salary'), fn($query) => $query->where('salary', '<=', request('max_salary')))
-            ->when(request('experience'), fn($query) => $query->where('experience', request('experience')))
-            ->when(request('category'), fn($query) => $query->where('category', request('category')));
-
-        return view('jobs.index',['jobs' => $jobs->get()]);
+        $filters = request()->only('search', 'min_salary', 'max_salary', 'experience', 'category');
+        return view('jobs.index',['jobs' => Job::filter($filters)->get()]);
     }
 
     /**
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,4 +14,18 @@ class Job extends Model
     //as it is static can can use without instance
     public static array $experience = ['Entry','Junior','Senior'];
     public static array  $category = ['IT','Finance','Sales','Marketing'];
+
+
+    public function scopeFilter(Builder | QueryBuilder $query, array $filters) : Builder|QueryBuilder
+    {
+        return $query->when($filters['search'] ?? null , function($query, $search){
+            $query->where( function($query) use ($search){
+                $query->where('title','like', '%'.$search.'%')
+                    ->orWhere('description','like','%'.$search.'%');
+            });
+        })->when($filters['min_salary'] ?? null, fn($query, $minSalary) => $query->where('salary','>=', $minSalary)
+            )->when($filters['max_salary'] ?? null, fn($query, $maxSalary) => $query->where('salary', '<=', $maxSalary))
+            ->when($filters['experience'] ?? null, fn($query, $experience) => $query->where('experience', $experience))
+            ->when($filters['category'] ?? null, fn($query, $category) => $query->where('category', $category));
+    }
 }
