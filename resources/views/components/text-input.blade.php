c79298c8e663e04dd7c3e1b15d1585295a9068e2
NancyMile <smithnancy@outlook.com>
2024-02-15 10:23:01 +1100

    auth sign-in form

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -6,6 +6,6 @@
             </svg>
         </button>
     @endif
-    <input  x-ref="input-{{ $name }}" type="text" placeholder="{{ $placeholder }}" name="{{ $name }}" value="{{ $value }}"  id="{{ $name }}"
+    <input  x-ref="input-{{ $name }}" type={{ $type }} placeholder="{{ $placeholder }}" name="{{ $name }}" value="{{ $value }}"  id="{{ $name }}"
 class="w-full round rounded-md  border-0 py-1.5 px-2.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2 pr-8"/>
 </div>
